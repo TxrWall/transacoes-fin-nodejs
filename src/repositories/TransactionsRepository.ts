0c2b327b871c2e1a5df21180e2c8e3eb86f10b31
Wallace <wllcblm@gmail.com>
2020-11-10 21:03:28 -0300

    Challange finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,24 +6,58 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
   constructor() {
-    this.transactions = [];
+    this.transactions = []
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch(transaction.type) {
+          case 'income':
+          accumulator.income += transaction.value
+          break;
+          case 'outcome':
+          accumulator.outcome += transaction.value
+          break;
+          default:
+          break;
+        }
+
+        return accumulator
+      },
+      {
+        income:0,
+        outcome:0,
+        total:0
+      })
+
+      const total = income - outcome
+
+      return { income, outcome, total }
+    }
+
+    public create({ title, value, type }: CreateTransaction): Transaction {
+      const transaction = new Transaction({ title, value, type })
+
+      this.transactions.push(transaction)
+
+      return transaction
+    }
   }
 
-  public create(): Transaction {
-    // TODO
-  }
-}
-
-export default TransactionsRepository;
+  export default TransactionsRepository;
